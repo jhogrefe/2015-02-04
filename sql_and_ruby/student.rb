567e7af50c3ca32fa8146c555506c7303d40c94e
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-04 15:13:17 -0600

    Updated Student file with pry commands and fixed id variable.

--- sql_and_ruby/student.rb
+++ sql_and_ruby/student.rb
@@ -1,14 +1,22 @@
+# @id   - Integer
+# @name - String
+
 class Student
   
   def initialize(options)
+    @id   = options[:id]
     @name = options[:name]
   end
   
   def save
     # In SQL, the command would look like: 
-    # INSERT INTO students (name) VALUES ("Andrew");
+    # INSERT INTO students (id, name) VALUES (1, "Andrew");
     
-    DATABASE.execute("INSERT INTO students (name) VALUES (\"#{@name}\")")
+    DATABASE.execute("INSERT INTO students (id, name) VALUES (#{@id}, \"#{@name}\")")
   end
     
 end
+
+# in pry:
+# andrew = Student.new({id: 1, name: "Andrew"})
+# beth = Student.new({id: 2, name: "Beth"})
